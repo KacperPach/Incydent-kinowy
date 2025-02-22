c5fc6115932f5d174d154ba9eb25b4f21b8253de
KacperPach <39103506+KacperPach@users.noreply.github.com>
2025-02-22 19:07:43 +0100

    added wokflow 📋\

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://KacperPach.github.io',
+	base: 'Incydent-kinowy',
 	integrations: [mdx(), sitemap()],
 });
